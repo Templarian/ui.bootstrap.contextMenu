1eda271dd85440f863fc309ee94b252c8ca74dae
Austin Andrews <git@templarian.com>
2014-10-16 01:21:56 -0500

    Module.

---
+++ contextMenu.js
@@ -0,0 +1,67 @@
+﻿angular.module('ui.bootstrap.contextMenu', [])
+
+.directive('contextMenu', function ($parse) {
+    var renderContextMenu = function ($scope, event, options) {
+        if (!$) { var $ = angular.element; }
+        $(event.currentTarget).addClass('context');
+        var $contextMenu = $('<div>');
+        $contextMenu.addClass('dropdown clearfix');
+        var $ul = $('<ul>');
+        $ul.addClass('dropdown-menu');
+        $ul.attr({ 'role': 'menu' });
+        $ul.css({
+            display: 'block',
+            position: 'absolute',
+            left: event.pageX + 'px',
+            top: event.pageY + 'px'
+        });
+        angular.forEach(options, function (item, i) {
+            var $li = $('<li>');
+            if (item === null) {
+                $li.addClass('divider');
+            } else {
+                $a = $('<a>');
+                $a.attr({ tabindex: '-1', href: '#' });
+                $a.text(typeof item[0] == 'string' ? item[0] : item[0].call($scope, $scope));
+                $li.append($a);
+                $li.on('click', function () {
+                    $scope.$apply(function () {
+                        item[1].call($scope, $scope);
+                    });
+                });
+            }
+            $ul.append($li);
+        });
+        $contextMenu.append($ul);
+        $contextMenu.css({
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            zIndex: 9999
+        });
+        $(document).find('body').append($contextMenu);
+        $contextMenu.on("click", function (e) {
+            $(event.currentTarget).removeClass('context');
+            $contextMenu.remove();
+        }).on('contextmenu', function (event) {
+            $(event.currentTarget).removeClass('context');
+            event.preventDefault();
+            $contextMenu.remove();
+        });
+    };
+    return function ($scope, element, attrs) {
+        element.on('contextmenu', function (event) {
+            $scope.$apply(function () {
+                event.preventDefault();
+                var options = $scope.$eval(attrs.contextMenu);
+                if (options instanceof Array) {
+                    renderContextMenu($scope, event, options);
+                } else {
+                    throw '"' + attrs.contextMenu + '" not an array';
+                }
+            });
+        });
+    };
+});
