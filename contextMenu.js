82ba7d0a72ffa5431d5e1b3081859b5018e8f440
Pem Taira <jose_taira@azeus.com>
2016-01-08 06:54:49 +0000

    Added functionality to display the nested menu on mouseover

--- contextMenu.js
+++ contextMenu.js
@@ -4,16 +4,30 @@ angular.module('ui.bootstrap.contextMenu', [])
 
     var contextMenus = [];
 
-    var removeContextMenus = function() {
-      while(contextMenus.length) {
+    var removeContextMenus = function(level) {
+      while(contextMenus.length && (!level || contextMenus.length > level)) {
         contextMenus.pop().remove();
+      }
+
+      if(contextMenus.length == 0 && $currentContextMenu) {
+        $currentContextMenu.remove();
       }
     };
 
-    var renderContextMenu = function ($scope, event, options, model) {
+    var $currentContext
+
+    var $currentContextMenu = null;
+
+    var renderContextMenu = function ($scope, event, options, model, level) {
+        if (!level) { level = 0; }
         if (!$) { var $ = angular.element; }
         $(event.currentTarget).addClass('context');
         var $contextMenu = $('<div>');
+        if($currentContextMenu) {
+            $contextMenu = $currentContextMenu;
+        } else {
+            $currentContextMenu = $contextMenu;
+        }
         $contextMenu.addClass('dropdown clearfix');
         var $ul = $('<ul>');
         $ul.addClass('dropdown-menu');
@@ -22,7 +36,8 @@ angular.module('ui.bootstrap.contextMenu', [])
             display: 'block',
             position: 'absolute',
             left: event.pageX + 'px',
-            top: event.pageY + 'px'
+            top: event.pageY + 'px',
+            "z-index": 10000
         });
         angular.forEach(options, function (item, i) {
             var $li = $('<li>');
@@ -30,6 +45,7 @@ angular.module('ui.bootstrap.contextMenu', [])
                 $li.addClass('divider');
             } else {
                 var $a = $('<a>');
+                $a.css("padding-right", "8px");
                 $a.attr({ tabindex: '-1', href: '#' });
                 var text = typeof item[0] == 'string' ? item[0] : item[0].call($scope, $scope, event, model);
                 $q.when(text).then(function(text) {
@@ -43,16 +59,28 @@ angular.module('ui.bootstrap.contextMenu', [])
                 var enabled = angular.isFunction(item[2]) ? item[2].call($scope, $scope, event, text, model) : true;
                 if (enabled) {
                     $li.on('click', function ($event) {
-                        $event.preventDefault();
+                        //$event.preventDefault();
                         $scope.$apply(function () {
                             if(angular.isArray(item[3])) {
+                                removeContextMenus(level + 1);
                                 $event.pageX = event.pageX + $ul.width();
                                 $event.pageY = $li.offset().top - 3;
-                                renderContextMenu($scope, $event, item[3], model);
+                                renderContextMenu($scope, $event, item[3], model, level + 1);
                             } else {
                                 $(event.currentTarget).removeClass('context');
                                 removeContextMenus();
                                 item[1].call($scope, $scope, event, model);
+                            }
+                        });
+                    });
+
+                    $li.on('mouseover', function ($event) {
+                        $scope.$apply(function () {
+                            if(angular.isArray(item[3])) {
+                                removeContextMenus(level + 1);
+                                $event.pageX = event.pageX + $ul.width();
+                                $event.pageY = $li.offset().top - 3;
+                                renderContextMenu($scope, $event, item[3], model, level + 1);
                             }
                         });
                     });
@@ -81,6 +109,7 @@ angular.module('ui.bootstrap.contextMenu', [])
         });
         $(document).find('body').append($contextMenu);
         $contextMenu.on("mousedown", function (e) {
+            console.log(e);
             if ($(e.target).hasClass('dropdown')) {
                 $(event.currentTarget).removeClass('context');
                 removeContextMenus();
@@ -88,13 +117,13 @@ angular.module('ui.bootstrap.contextMenu', [])
         }).on('contextmenu', function (event) {
             $(event.currentTarget).removeClass('context');
             event.preventDefault();
-            removeContextMenus();
+            removeContextMenus(level);
         });
         $scope.$on("$destroy", function() {
             removeContextMenus();
         });
 
-        contextMenus.push($contextMenu);
+        contextMenus.push($ul);
     };
     return function ($scope, element, attrs) {
         element.on('contextmenu', function (event) {
